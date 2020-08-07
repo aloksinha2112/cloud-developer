3a47255324af31d82de0f6bc0575db9a2050071c
Alok Sinha <Alok.Sinha@shell.com>
2020-08-07 13:14:15 +0530

    Own Instagram on AWS

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -25,10 +25,20 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // QUERY PARAMATERS
   //    image_url: URL of a publicly accessible image
   // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]  
+  /**************************************************************************** */ 
+  app.get("/filteredimage", async (req, res) => 
+  {
+      const { image_url } = req.query; 
+      if (!image_url) { res.status(400).send("Image URL is required"); } 
+      try {        
+        const filteredImg = await filterImageFromURL(image_url); 
+        res.status(200).sendFile(filteredImg, async () => { 
+        await deleteLocalFiles([filteredImg]); 
+        }); 
+      } catch (error) { res.status(415).send(error); }
+  } );
+  
   //! END @TODO1
   
   // Root Endpoint
