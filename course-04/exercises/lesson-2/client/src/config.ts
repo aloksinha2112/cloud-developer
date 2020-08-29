12625b82772175297ff21f206d7c89d4d52ec2be
Alok Sunha <alok.sinha2112@gmail.com>
2020-08-29 16:15:09 +0530

    Course4_lesson4

--- course-04/exercises/lesson-2/client/src/config.ts
+++ course-04/exercises/lesson-2/client/src/config.ts
@@ -1,1 +1,1 @@
-export const apiEndpoint = '...'
+export const apiEndpoint = 'https://kw9cwowdeg.execute-api.us-east-2.amazonaws.com/dev'
--- course-04/exercises/lesson-4/client/src/config.ts
+++ course-04/exercises/lesson-4/client/src/config.ts
@@ -1,1 +1,1 @@
-export const apiEndpoint = '...'
+export const apiEndpoint = 'https://sg35kr95vl.execute-api.us-east-2.amazonaws.com/dev'
