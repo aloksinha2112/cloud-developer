dd4dd87b2e9c9039aef4231e9f6a1f7b89e2728c
Alok Sunha <alok.sinha2112@gmail.com>
2020-08-30 17:39:37 +0530

    lesson 4 done

--- course-04/exercises/lesson-6/client/src/config.ts
+++ course-04/exercises/lesson-6/client/src/config.ts
@@ -1,7 +1,7 @@
-export const apiEndpoint = '...'
+export const apiEndpoint = 'https://4d7uoytqrf.execute-api.us-east-2.amazonaws.com/dev'
 
 export const authConfig = {
-  domain: '...',
-  clientId: '...',
-  callbackUrl: 'http://localhost:3000/callback'
+    domain: 'dev-bscw7p53.us.auth0.com',
+    clientId: 'ZDrs3vhmQtowqIe3TViMoxc2NnKN1tfc',
+    callbackUrl: 'http://localhost:3000/callback'
 }
--- course-04/exercises/lesson-6/solution/src/lambda/auth/rs256Auth0Authorizer.ts
+++ course-04/exercises/lesson-6/solution/src/lambda/auth/rs256Auth0Authorizer.ts
@@ -5,7 +5,25 @@ import 'source-map-support/register'
 import { verify } from 'jsonwebtoken'
 import { JwtToken } from '../../auth/JwtToken'
 
-const cert = `...`
+const cert = `-----BEGIN CERTIFICATE-----
+MIIDDTCCAfWgAwIBAgIJJOoYD5zqaVFWMA0GCSqGSIb3DQEBCwUAMCQxIjAgBgNV
+BAMTGWRldi1ic2N3N3A1My51cy5hdXRoMC5jb20wHhcNMjAwODI5MTE1OTU1WhcN
+MzQwNTA4MTE1OTU1WjAkMSIwIAYDVQQDExlkZXYtYnNjdzdwNTMudXMuYXV0aDAu
+Y29tMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA+Bv68o+bAlZ38rr2
+kv9XEW+z1b213Opcxw5Jj6K+9Nt6FxNAnsPy9I9ZofTepLcGmV3WLy3pLHttY62f
+voEO7TXVMYpUANgUBP8webBmj3emHzUBSkxo2ekwebtppqpEwrJzBZz7QlAGxde6
+qQUqIoL96ytGrKUsbS0YUYUeCYs8yz0Ytj3dWy2AltGURe/xGYn7PSGqMNIXCiJP
+8RCO7RuOfFtcpa6JmK1D3TG7armtdJZ0AQHgk/cLnPf5RXyQ2QlURxS8neCfl4dt
+SLVz9otsNSR4PHMDXJuVEAsfg8MaIyTQp3PRrdbLZmAZ5DMV7vMn8peS4bAD+mQI
+Ni5OPQIDAQABo0IwQDAPBgNVHRMBAf8EBTADAQH/MB0GA1UdDgQWBBQGh5s1NTAX
+b9rkBOH6FLOty52T2zAOBgNVHQ8BAf8EBAMCAoQwDQYJKoZIhvcNAQELBQADggEB
+AAu6wNwFw9tvALJXUOUUy+nG4EVKiJqJ7uT8Rkf2AGm4g+oJMDBKEtlmTzqScwfx
+E8CBOSYVfCBVjmxmEJvA+paT8xYroAsEW3WxuewPcKknfK7l6xxmdH1nTNvCVzo9
+FxzP4RYTeupZncLj0FaKfnPPZKhURY3ccRDysSOKogljZCHxmUiSKu8oz3qWJJX+
+gBp0Ri/VlWaTf579VaLAn3ND3jXmlwwqdIaQLDRbdfXHWzjzOg0l8cw8EuoWIGAS
+W53M4hfW0ecpqbb70SEXZzbEs19zeaROjzmvfNogAR1+QnjgwvAwrJGuvAjEnMEh
+nAsul6Y18Zc9MVAqurmuixM=
+-----END CERTIFICATE-----`
 
 export const handler = async (event: CustomAuthorizerEvent): Promise<CustomAuthorizerResult> => {
   try {
