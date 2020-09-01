81a6b36578f7f5b18ff422992a0caa9f8dff7599
Alok Sunha <alok.sinha2112@gmail.com>
2020-09-01 21:04:00 +0530

    proj5_submission

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,10 +1,8 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
-
 import { verify } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
 import Axios from 'axios'
-//import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
@@ -56,13 +54,11 @@ export const handler = async (
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
-  //const jwt: Jwt = decode(token, { complete: true }) as Jwt
-
+  
   // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  //const { header } = jwt;
-  //let key = await getSigningKey(jwksUrl, header.kid)
+  
   const cert = await getCertificate()
   return verify(token, cert, { algorithms: ['RS256'] }) as JwtPayload
 }
@@ -86,33 +82,4 @@ async function getCertificate(): Promise<string> {
 
   return cert
 }
-// const getSigningKey = async (jwkurl, kid) => {
-//   let res = await Axios.get(jwkurl, {
-//     headers: {
-//       'Content-Type': 'application/json',
-//       "Access-Control-Allow-Origin": "*",
-//       'Access-Control-Allow-Credentials': true,
-//     }
-//   });
-//   let keys  = res.data.keys;
-//   // since the keys is an array its possible to have many keys in case of cycling.
-//   const signingKeys = keys.filter(key => key.use === 'sig' // JWK property `use` determines the JWK is for signing
-//       && key.kty === 'RSA' // We are only supporting RSA
-//       && key.kid           // The `kid` must be present to be useful for later
-//       && key.x5c && key.x5c.length // Has useful public keys (we aren't using n or e)
-//     ).map(key => {
-//       return { kid: key.kid, nbf: key.nbf, publicKey: certToPEM(key.x5c[0]) };
-//     });
-//   const signingKey = signingKeys.find(key => key.kid === kid);
-//   if(!signingKey){
-//     throw new Error('Invalid signing keys')
-//     logger.error("No signing keys found")
-//   }
-//   logger.info("Signing keys created successfully ", signingKey)
-//   return signingKey
-// };
-// function certToPEM(cert) {
-//   cert = cert.match(/.{1,64}/g).join('\n');
-//   cert = `-----BEGIN CERTIFICATE-----\n${cert}\n-----END CERTIFICATE-----\n`;
-//   return cert;
-// }
+
